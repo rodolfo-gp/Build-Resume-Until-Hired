0e359aebf138ff3c3448a4763e5ed522353b2c67
FahmiSar <fahmi.sardar@ucalgary.ca>
2025-02-24 11:10:56 -0700

    Login page backbone
    Login page is now made with fields, submit button and a continue as guest button which sends the user to the Homepage.

--- presentationLayer/react-app/src/pages/Loginpage.js
+++ presentationLayer/react-app/src/pages/Loginpage.js
@@ -1,32 +1,60 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import Header from "../components/Header";
 
 import "../styles/Loginpage.css";
 
-function Loginpage(){
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+function Loginpage() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-    return(
-        <form className = "loginpage-container">
-            <input
-                type = "text"
-                value={username}
-                onChange = {(event) => setUsername(event.target.value)}
-                placeholder = "Enter username..."
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                placeholder="Enter password..."
-            />
-            <p>Username: {username}</p>
-        </form>
-    );
+	/** This is how we send user login information to the flask backend
+	 *  TODO: Change the url to be the actual URL required for the backend
+	 */
+	const url = "http://127.0.0.1:5000/submit";
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({
+				username: username,
+				password: password,
+			}),
+		});
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="loginpage-container">
+			<div className = "username-container">
+                <input
+                    type = "text"
+                    value = {username}
+                    onChange = {(event) => setUsername(event)}
+                    placeholder = "Enter username..."
+                />
+            </div>
+            <div className = "password-container">
+                <input
+                    type = "password"
+                    value = {password}
+                    onChange={(event) => setPassword(event)}
+                    placeholder="Enter password..."
+                />
+            </div>
+
+			<button className="submit button" type="submit">
+				Submit
+			</button>
+			<button className="guest button" onClick ={() => window.location.href = "/Homepage"}>
+                Continue as Guest
+            </button>
+		</form>
+	);
 }
-
-
 
 export default Loginpage;
