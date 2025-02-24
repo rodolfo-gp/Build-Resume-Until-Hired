51f0c666d2da19fa1bfa7dd302764d95f7a24a2a
Alexander <elegray888@hotmail.com>
2025-02-24 11:18:53 -0700

    Fixed href in homepage

--- presentationLayer/react-app/src/pages/Homepage.js
+++ presentationLayer/react-app/src/pages/Homepage.js
@@ -9,10 +9,10 @@ function Homepage(){
         <div className = "homepage-container">
             <h1>Which would you like to create</h1>
             <div className="Buttons">
-            <a href="Resume.html">  
+            <a href="Resume">  
                 <button name="Resume">Resume</button>
             </a> 
-            <a href="Cover_Letter.html">
+            <a href="Cover_Letter">
                 <button name="Cover Letter">Cover Letter</button>
             </a>
             </div>
