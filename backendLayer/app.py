2750eae77a7feb539097a8b5ea73d4a901b3c761
Mohammed Zaid Shaikh <mohammed.shaikh1@ucalgary.ca>
2025-03-18 08:57:40 -0600

    testing the coverletter output

--- backendLayer/app.py
+++ backendLayer/app.py
@@ -4,6 +4,9 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 import os
 
+from applicationMaterial import (CoverLetter, Resume)
+from llm.gptPromptingutilities import gpt_prompter
+
 app = Flask(__name__)
 CORS(app)
 
@@ -21,13 +24,13 @@ def signup():
         data = request.json
         if not data or "email" not in data or "password" not in data:
             return "Bad Request, Missing Data", 400
-        
+
         hashed_email = ch.hash_email(data["email"])
 
         if not does_user_exist(hashed_email):
             hashed_password = ch.hash_password(data["password"])
             newUser = {"email": hashed_email, "password": hashed_password}
-            
+
             try:
                 mongo.db.users.insert_one(newUser)
                 return "User Created Successfully", 201
@@ -53,7 +56,7 @@ def login():
         data = request.json
         if not data or "email" not in data or "password" not in data:
             return "Bad Request, Missing Data", 400
-        
+
         hashed_email = ch.hash_email(data["email"])
         if does_user_exist_login(hashed_email, data["password"]):
             return "Successful Login", 201
@@ -62,3 +65,17 @@ def login():
     except Exception as e:
         print(f"Error: {e}")
         return "Bad Request", 500
+
+@app.route('/coverletter', methods=['POST'])
+def generate_coverletter():
+    data = request.json
+    cover_letter = CoverLetter(data)
+    prompt = cover_letter.createCoverLetterPrompt()
+    output = gpt_prompter(prompt)
+    print(output)
+    return
+
+@app.route('/resume', method=['POST'])
+def generate_resume():
+
+    return
