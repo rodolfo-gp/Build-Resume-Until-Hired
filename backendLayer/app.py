ea794a05933b1bc95505b8af9bd0c8e55af9df16
Inko-z <rodolfoandresgp@hotmail.com>
2025-03-18 11:31:07 -0600

    Updated login and singupreturne requests

--- backendLayer/app.py
+++ backendLayer/app.py
@@ -20,11 +20,17 @@ def test():
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    return_request = {
+        "message": "",
+        "status": False
+    }
     try:
         data = request.json
         if not data or "email" not in data or "password" not in data:
-            return "Bad Request, Missing Data", 400
-
+            
+            return_request["message"] = "Bad Request, Missing Data"
+            return jsonify(return_request), 400
+        
         hashed_email = ch.hash_email(data["email"])
 
         if not does_user_exist(hashed_email):
@@ -33,15 +39,21 @@ def signup():
 
             try:
                 mongo.db.users.insert_one(newUser)
-                return "User Created Successfully", 201
+                return_request["message"] = "User Created Successfully"
+                return_request["status"] = True
+                return jsonify(return_request), 201
+            
             except Exception as e:
                 print(f"Database Error: {e}")
-                return "Database Insertion Error", 501
+                return_request["message"] = "Database Insertion Error"
+                return jsonify(return_request), 501
         else:
-            return "User with that email already exists", 402
+            return_request["message"] = "User with that email already exists"
+            return jsonify(return_request), 400
     except Exception as e:
         print(f"Error: {e}")
-        return "Bad Request", 500
+        return_request["message"] = e
+        return jsonify(return_request), 500
 
 def does_user_exist(email):
     return mongo.db.users.find_one({"email": email}, {"_id": 0}) is not None
@@ -66,7 +78,7 @@ def login():
         if does_user_exist_login(hashed_email, data["password"]):
             return_request["message"] = "Successful Login"
             return_request["status"] = True
-            return jsonify(return_request), 201
+            return jsonify(return_request), 200
         else:
             return_request["message"] = "Incorrect email or password"
             return jsonify(return_request), 400
