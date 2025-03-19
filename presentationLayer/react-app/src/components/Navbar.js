da125b0d84c832220f5e383c6dea726b5e300be8
Alexander <elegray888@hotmail.com>
2025-03-19 11:40:26 -0600

    removed conditional functionality of view saved button

--- presentationLayer/react-app/src/components/Navbar.js
+++ presentationLayer/react-app/src/components/Navbar.js
@@ -1,17 +1,7 @@
-import React, {useState,useEffect} from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 function Navbar() {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-
-  const [isloggedin, setLoggedin] = useState(false);
-  useEffect(()=>{
-    setEmail(localStorage.getItem("email"))
-    setPassword(localStorage.getItem("password"))
-    setLoggedin(email != null && password != null && email != "" && password != "")
-  },[isloggedin, email, password])
 
    
   return (
@@ -22,7 +12,7 @@ function Navbar() {
         <li><Link to="/Homepage">Homepage</Link></li>
         <li><Link to="/Cover_Letter">Cover Letter</Link></li>
         <li><Link to="/Resume">Resume</Link></li>
-        {isloggedin && <li><Link to="/View">View Saved</Link></li>}
+        <li><Link to="/View">View Saved</Link></li>
       </ul>
     </nav>
   );
