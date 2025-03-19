74bb51209632b6ff3d1f32ff9632c501bfa1a19b
Alexander <elegray888@hotmail.com>
2025-03-19 10:30:10 -0600

    added conditional view saved button

--- presentationLayer/react-app/src/components/Navbar.js
+++ presentationLayer/react-app/src/components/Navbar.js
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 function Navbar() {
+  const email = localStorage.getItem("email")
+
+  let isloggedin = (email != null)
   return (
     <nav className="navbar">
       <ul>
@@ -10,6 +13,7 @@ function Navbar() {
         <li><Link to="/Homepage">Homepage</Link></li>
         <li><Link to="/Cover_Letter">Cover Letter</Link></li>
         <li><Link to="/Resume">Resume</Link></li>
+        {isloggedin && <li><Link to="/View">View Saved</Link></li>}
       </ul>
     </nav>
   );
