fc130d47bf4b579752afbce307e9a9d4e83d2a5c
Alexander <elegray888@hotmail.com>
2025-03-19 13:32:00 -0600

    added save button

--- presentationLayer/react-app/src/pages/Output.js
+++ presentationLayer/react-app/src/pages/Output.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useLocation } from 'react-router-dom';
 
 import "../styles/Generation.css";
@@ -6,7 +6,42 @@ import "../styles/Generation.css";
 const OutputForm = () => {
     // Retrieve state passed via navigate
     const location = useLocation();
-    const { output, doc_title } = location.state || {};
+    let { output, doc_title } = location.state || {};
+    const [responsemessage, setMessage] = useState("");
+    if (localStorage.getItem("doc_body") !== undefined){    
+        output = JSON.parse(localStorage.getItem("doc_body"));    
+        localStorage.setItem("doc_body", JSON.stringify(output));
+        localStorage.setItem("doc_title", JSON.stringify(doc_title));
+    }
+
+
+    const email = localStorage.getItem("email")
+    const password = localStorage.getItem("password")
+
+    const URL = "https://api.bru-h.xyz/cv/save"
+    
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+
+    async function Savedoc(){
+        await fetch(URL, {
+            method: "POST",
+			headers: myHeaders,
+			body: JSON.stringify({
+                email:email,
+                password:password,
+                latex:false,
+                doc_body:output,
+                doc_title:doc_title
+            }),
+        }).then((response)=>{
+			if (response.status >= 200 && response.status < 300) {
+                setMessage("Save Successful")
+			}else{
+                setMessage("Save failed")
+            }
+        })
+    }
 
     return (
         <div className='output_page'>
@@ -19,6 +54,10 @@ const OutputForm = () => {
                 ) : (
                     <p>No output available</p>
                 )}
+            </div>
+            <div className='button&response'>
+            {email && password && <button onClick={()=>Savedoc()}>Save</button>}
+                {responsemessage}
             </div>
         </div>
     );
