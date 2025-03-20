9bfd2022f0de9328dacf67ae8b46f08eb408bd8d
Mohammed Zaid Shaikh <mohammed.shaikh1@ucalgary.ca>
2025-03-19 21:25:11 -0600

    Navbar and Header combined and image added to Navbar

--- presentationLayer/react-app/src/components/Navbar.js
+++ presentationLayer/react-app/src/components/Navbar.js
@@ -1,21 +1,26 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
+
 function Navbar() {
 
 
   return (
-    <nav className="navbar">
-      <ul>
-        <li><Link to="/Login">Login Page</Link></li>
-        <li><Link to="/Signup">Signup Page</Link></li>
-        <li><Link to="/Homepage">Homepage</Link></li>
-        <li><Link to="/Cover_Letter">Cover Letter</Link></li>
-        <li><Link to="/Resume">Resume</Link></li>
-        <li><Link to="/Output">Generation Output</Link></li>
-        <li><Link to="/View">View Saved</Link></li>
-      </ul>
-    </nav>
+    <div className="header-container">
+      <nav className="navbar">
+
+        <ul>
+          <img src="/images/bruh_logo.png" alt="Logo" />
+          <li><Link to="/Login">Login Page</Link></li>
+          <li><Link to="/Signup">Signup Page</Link></li>
+          <li><Link to="/Homepage">Homepage</Link></li>
+          <li><Link to="/Cover_Letter">Cover Letter</Link></li>
+          <li><Link to="/Resume">Resume</Link></li>
+          <li><Link to="/Output">Generation Output</Link></li>
+          <li><Link to="/View">View Saved</Link></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
