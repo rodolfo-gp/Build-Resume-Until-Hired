4ab2f66cc6c668de98eb3d5ecf8e39a7017b4f3a
Alexander <elegray888@hotmail.com>
2025-03-20 14:31:25 -0600

    failed

--- presentationLayer/react-app/src/components/Navbar.js
+++ presentationLayer/react-app/src/components/Navbar.js
@@ -8,24 +8,29 @@ import { useUser } from "../context/UserContext";
 function Navbar() {
   const { email, password, login, logout } = useUser();
   const [logged, setLogged] = useState(false);
-
+  let test = false
   useEffect(() => {
-    setLogged(logged)
+    test = true
     }, [email]);
+
+    function Logouthandler(){
+      logout()
+      setLogged(false)
+    }
 
   return (
     <div className="header-container">
       <img src="/images/bruh_logo.png" alt="Logo" />
       <nav className="navbar">
         <ul>
-          {logged ? (<li><Link to="/Homepage">Login Page</Link></li>) : (<li><Link to="/Login">Login Page</Link></li>)}
-          {logged ? (<li><Link to="/Homepage">Signup Page</Link></li>) : (<li><Link to="/Signup">Signup Page</Link></li>)}
+          {test ? (<li><Link to="/Homepage">Login Page</Link></li>) : (<li><Link to="/Login">Login Page</Link></li>)}
+          {test ? (<li><Link to="/Homepage">Signup Page</Link></li>) : (<li><Link to="/Signup">Signup Page</Link></li>)}
           <li><Link to="/Homepage">Homepage</Link></li>
           <li><Link to="/Cover_Letter">Cover Letter</Link></li>
           <li><Link to="/Resume">Resume</Link></li>
           <li><Link to="/Output">Generation Output</Link></li>
           <li><Link to="/View">View Saved</Link></li>
-          {logged && <li><button onClick={logout()}>Logout</button></li>}
+          {test && <li><button onClick={Logouthandler()}>Logout</button></li>}
         </ul>
       </nav>
     </div>
